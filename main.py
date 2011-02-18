b1d98dd19c4df86052437ed8caf22f9db95c02f6
Roman Klimenko <romaklimenko@gmail.com>
2011-02-19 00:12:55 +0200

    Quickly reworked sample solution. Now users can add URLs. Each user can see their URLs only. Also remember about fetch(10).

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
+import cgi
+
+
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp.util import run_wsgi_app
 
+class UrlItem(db.Model):
+	owner = db.UserProperty()
+	url = db.StringProperty()
+	date = db.DateTimeProperty(auto_now_add=True)
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		query = UrlItem.all()
+		query.filter("owner =", users.get_current_user())
+		query.order("-date")
+		url_items = query.fetch(10)
 
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+
+		template_values = {
+			'url_items': url_items,
+			'url': url,
+			'url_linktext': url_linktext,
+		}
+
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
+
+class UrlItemsController(webapp.RequestHandler):
+	def post(self):
+		url_item = UrlItem()
+
+		if users.get_current_user():
+			url_item.owner = users.get_current_user()
+
+		url_item.url = self.request.get('url')
+		url_item.put()
+		self.redirect('/')
+
+application = webapp.WSGIApplication(
+									[('/', MainPage),
+									('/new', UrlItemsController)],
+									debug=True)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+	run_wsgi_app(application)
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	main()
